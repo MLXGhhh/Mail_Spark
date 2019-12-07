e7679586acd15591fc7a0edc6efd92d96877cea8
MLXGhhh <hankuiyu0315@163.com>
2019-12-07 17:17:24 +0800

    Initial commit

---
+++ kafkaTest/src/main/scala/cnic/xiandao/KafkaConsumerDemo.scala
@@ -0,0 +1,39 @@
+package cnic.xiandao
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+
+/**
+  *@Description SparkStreaming消费kafka数据 测试代码
+  *@Author hky
+  **/
+object KafkaConsumerDemo {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Consumer").set("spark.streaming.kafka.consumer.poll.ms", "10000").setMaster("local[*]")
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
+    val topic = Set("kafka-action")
+    val broker = "106.39.31.27:9092"
+    val kafkaPrams = Map[String,Object](ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> broker,
+                                        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+                                        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+                                        ConsumerConfig.GROUP_ID_CONFIG -> "test",
+                                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest",
+                                        ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: java.lang.Boolean))
+    val stream: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+                                                                          ssc,
+                                                                          PreferConsistent,
+                                                                          Subscribe[String, String](topic, kafkaPrams))
+    //val value: DStream[String] = stream.map(x => x.value)
+    stream.saveAsTextFiles("F:\\kafka-consumer")
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
